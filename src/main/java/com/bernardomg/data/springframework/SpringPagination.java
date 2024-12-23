b6ee0d9714a7dae5585cf29db859c9c28238caa1
Bernardo-MG <programming@bernardomg.com>
2024-12-24 00:40:50 +0100

    Pagination creation

---
+++ src/main/java/com/bernardomg/data/springframework/SpringPagination.java
@@ -0,0 +1,55 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2023 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.bernardomg.data.springframework;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import com.bernardomg.data.domain.Pagination;
+import com.bernardomg.data.domain.Sorting;
+
+/**
+ * Utilities to transform {@link Pagination} into {@link Pageable}.
+ * <p>
+ * Spring pages start with index 0, so the page number has to be corrected.
+ *
+ * @author Bernardo Mart&iacute;nez Garrido
+ *
+ */
+public final class SpringPagination {
+
+    public static final Pageable toPageable(final Pagination pagination) {
+        return PageRequest.of(pagination.page() - 1, pagination.size());
+    }
+
+    public static final Pageable toPageable(final Pagination pagination, final Sorting sorting) {
+        final Sort sort;
+
+        sort = SpringSorting.toSort(sorting);
+        return PageRequest.of(pagination.page() - 1, pagination.size(), sort);
+    }
+
+}
