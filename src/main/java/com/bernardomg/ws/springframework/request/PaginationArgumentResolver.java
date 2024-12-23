2c097dad2540ac22c089b2ec746e9f8ad590639e
Bernardo-MG <programming@bernardomg.com>
2024-12-24 00:03:54 +0100

    Cleaned up

--- src/main/java/com/bernardomg/ws/springframework/request/PaginationArgumentResolver.java
+++ src/main/java/com/bernardomg/ws/springframework/request/PaginationArgumentResolver.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2023 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.bernardomg.ws.springframework.request;
 
@@ -11,12 +34,26 @@ import com.bernardomg.data.domain.Pagination;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Argument resolver to acquire a {@link Pagination} from the request parameters. In case these parameters are missing, it applies
+ * default values.
+ *
+ * @author Bernardo Mart&iacute;nez Garrido
+ *
+ */
 @Slf4j
 public final class PaginationArgumentResolver implements HandlerMethodArgumentResolver {
 
     private static final int DEFAULT_PAGE = 1;
 
     private static final int DEFAULT_SIZE = 10;
+
+    /**
+     * Default constructor.
+     */
+    public PaginationArgumentResolver() {
+        super();
+    }
 
     @Override
     public final Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer,
@@ -28,6 +65,7 @@ public final class PaginationArgumentResolver implements HandlerMethodArgumentRe
         final int    parsedSize;
         final int    size;
 
+        // Parse page
         pageParam = webRequest.getParameter("page");
         if (pageParam == null) {
             page = DEFAULT_PAGE;
@@ -41,6 +79,7 @@ public final class PaginationArgumentResolver implements HandlerMethodArgumentRe
             }
         }
 
+        // Parse size
         sizeParam = webRequest.getParameter("size");
         if (sizeParam == null) {
             size = DEFAULT_SIZE;
